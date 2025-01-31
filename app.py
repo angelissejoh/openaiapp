f1d99fc27d77d6093db56a0b8d7bcda71e88ce28
Angelisse Johnson <angelissejoh2019@fau.edu>
2025-01-31 09:45:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-st.image("https://images.app.goo.gl/ww4Vhq6g87oimSNS8", caption="AI-Powered Streamlit App")
+image("https://images.app.goo.gl/ww4Vhq6g87oimSNS8", caption="AI-Powered Streamlit App")
 import streamlit as st
 import requests
 import json
